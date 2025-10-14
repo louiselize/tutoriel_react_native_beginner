02dd4d68cf5c6c3942d72cdb131da8dcef2afb5c
Louise Lizé <louise.lize@icloud.com>
2025-10-14 11:04:41 +0200

    feat: Add initial project setup with Prettier configuration and basic app structure

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import {Text } from 'react-native';
+import { Text } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Human } from './components/Human/Human';
 
 export default function App() {
   return (
-      <Text>Hello</Text>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Text>Hello</Text>
+        <Human />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-
