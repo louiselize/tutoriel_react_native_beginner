43af9ec921bab9ceffab950c6dce0bdb8975f52a
Louise Lizé <louise.lize@icloud.com>
2025-10-15 09:40:06 +0200

    feat: Style text with custom styles

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { Text } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from './App.style';
 import { Human } from './components/Human/Human';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <Text>Hello</Text>
+        <Text style={styles.helloText}>Hello</Text>
         <Human />
       </SafeAreaView>
     </SafeAreaProvider>
---
+++ App.style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  helloText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+    color: 'blue',
+  },
+});
